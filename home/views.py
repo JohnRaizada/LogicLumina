d25ac4c0ccce3dbcb15bd70751390c2c03e71a61
DivyamAgg24 <142659327+DivyamAgg24@users.noreply.github.com>
2023-10-13 12:06:14 +0530

    changed the text in homapageversion2

--- home/views.py
+++ home/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def HomePage(request):
-    return HttpResponse("<font color=\"red\"><h1>You are at homepage. Alas, I love you.</h1></font>")
+    return HttpResponse("<font color=\"red\"><h1>You are at homepage.</h1></font>")
 def HomePageVersion2(request):
-    return HttpResponse("<font color=\"blue\"><h6>You are at homepage. Sadly, I love you.</h6></font>")
+    return HttpResponse("<font color=\"blue\"><h6>You are at homepage. Reeeee.</h6></font>")
